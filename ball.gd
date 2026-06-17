5d27a777b79b06919cde30c85dde19bc417bd112
Vardaan <vardaan.bijnor@gmail.com>
2026-06-17 14:48:58 +0530

    fixed the error

--- ball.gd
+++ ball.gd
@@ -4,15 +4,23 @@ extends CharacterBody2D
 @export var multiplier : float = 1.5
 @export var max_speed = 600
 
-func _ready() -> void:
+func initalize():
+	var size = get_viewport().size
 	velocity = Vector2(-1, 0.25) * speed
+	var pos_x = size.x / 2
+	var pos_y = size.y / 2
+	position = Vector2(pos_x, pos_y)
+
+func _ready() -> void:
+	initalize()
 
 func _process(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
 	if collision != null :
 		var normal = collision.get_normal()
 		velocity = velocity.bounce(normal)
+
+func _on_speed_timer_timeout() -> void :
 	
-func _on_speed_timer_timeout() -> void:
 	velocity *= multiplier
 	velocity.limit_length(max_speed)
--- level.gd
+++ level.gd
@@ -1,20 +1,12 @@
 extends Node2D
 
-func _on_left_body_entered(body : CharacterBody2D) -> void:
-	#if body.name :
-		#Global.score_player_2 += 1
-		#print("p2 " + str(Global.score_player_2))
-		#print("p1 " + str(Global.score_player_1))
-	Global.score_player_2 += 1
-	print("p2 " + str(Global.score_player_2))
-	print("p1 " + str(Global.score_player_1))
+func _on_left_body_entered(body) -> void:
+	if body.is_in_group("ball") :
+		Global.score_player_2 += 1
 
 
-func _on_right_body_entered(body : CharacterBody2D) -> void:
-	#if body.is_in_group("ball") :
-		#Global.score_player_1 += 1
-		#print("p2 " + str(Global.score_player_2))
-		#print("p1 " + str(Global.score_player_1))
-	Global.score_player_1 += 1
-	print("p2 " + str(Global.score_player_2))
-	print("p1 " + str(Global.score_player_1))
+func _on_right_body_entered(body) -> void:
+	if body.is_in_group("ball") :
+		Global.score_player_1 += 1
+		get_tree().call_group("ball", "initialize")
+		
